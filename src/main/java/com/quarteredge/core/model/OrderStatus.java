0e87b560d5abaeb2f9c9dd4436c3b802b6c2133c
kingsimmons <kd3business@gmail.com>
2025-10-20 19:15:19 -0500

    QE-14: add Order and OrderStatus models, remove WhitespaceAround from checkstyle

---
+++ src/main/java/com/quarteredge/core/model/OrderStatus.java
@@ -0,0 +1,30 @@
+package com.quarteredge.core.model;
+
+/**
+ * Represents the status of an order in the trading system.
+ * <p>
+ * An order progresses through different states during its lifecycle,
+ * from initial creation to final closure.
+ * </p>
+ *
+ * @author QuarterEdge
+ * @version 1.0
+ * @since 1.0
+ */
+public enum OrderStatus {
+    /**
+     * Indicates that the order has been created but not yet activated.
+     * The order is waiting to be processed or submitted to the market.
+     * I.e., limit or stop orders
+     */
+    PENDING,
+    /**
+     * Indicates that the order is currently active in the market.
+     */
+    ACTIVE,
+    /**
+     * Indicates that the order has been closed.
+     * The order has reached its target, hit stop loss or expired.
+     */
+    CLOSED
+}
