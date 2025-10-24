c5b8ede59bf0586179086673fa67c45b0c237ae7
kingsimmons <kd3business@gmail.com>
2025-10-23 22:30:55 -0500

    QE-18: Create Ema indicator class

---
+++ src/main/java/com/quarteredge/core/indicator/EmaIndicator.java
@@ -0,0 +1,14 @@
+package com.quarteredge.core.indicator;
+
+public class EmaIndicator implements Indicator {
+    @Override
+    public void add() {}
+
+    @Override
+    public double get() {
+        return 0;
+    }
+
+    @Override
+    public void calculate() {}
+}
