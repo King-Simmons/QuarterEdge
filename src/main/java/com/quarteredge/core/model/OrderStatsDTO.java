96c3f64a57d7288e7d7a4b623e50e0d3a6d27440
kingsimmons <kd3business@gmail.com>
2025-12-25 18:14:57 -0600

    QE-54: Turn OrderStatsDTO into class instead of record

--- src/main/java/com/quarteredge/core/model/OrderStatsDTO.java
+++ src/main/java/com/quarteredge/core/model/OrderStatsDTO.java
@@ -1,3 +1,27 @@
 package com.quarteredge.core.model;
 
-public record OrderStatsDTO(double maximumFavorablePrice, double maximumAdversePrice) {}
+public class OrderStatsDTO {
+    private double maximumFavorablePrice;
+    private double maximumAdversePrice;
+
+    public OrderStatsDTO(double maximumFavorablePrice, double maximumAdversePrice) {
+        this.maximumFavorablePrice = maximumFavorablePrice;
+        this.maximumAdversePrice = maximumAdversePrice;
+    }
+
+    public double getMaximumFavorablePrice() {
+        return maximumFavorablePrice;
+    }
+
+    public double getMaximumAdversePrice() {
+        return maximumAdversePrice;
+    }
+
+    public void setMaximumFavorablePrice(double maximumFavorablePrice) {
+        this.maximumFavorablePrice = maximumFavorablePrice;
+    }
+
+    public void setMaximumAdversePrice(double maximumAdversePrice) {
+        this.maximumAdversePrice = maximumAdversePrice;
+    }
+}
